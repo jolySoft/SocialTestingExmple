3a99ef647a2f91a2e9dc3018201cc558da915523
jolyon.wharton@webenabled.tv <Bobafet1234>
2023-03-05 12:44:37 +0000

    Green but dirty
    To start with we'll build everything in the test.  We've got it
    green but we need to clean out the implementation from the test.

--- EventSourceTests/ShippingTest.cs
+++ EventSourceTests/ShippingTest.cs
@@ -1,13 +1,25 @@
-namespace EventSourceTests
-{
-    public class ShippingTest
-    {
-        [Fact]
-        public void CanCreateNewShip()
-        {
-            var ship = new Ship("Ever Given");
+using Shouldly;
 
-            Assert.
-        }
+namespace EventSourceTests;
+
+public class ShippingTest
+{
+    [Fact]
+    public void CanCreateNewShip()
+    {
+        var ship = new Ship("Ever Given");
+
+        ship.Name.ShouldBe("Ever Given");
+    }
+}
+
+public class Ship
+{
+    public string Name { get; }
+
+    public Ship(string name)
+    {
+        Name = name;
+
     }
 }
