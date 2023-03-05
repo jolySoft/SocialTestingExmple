02cd90aad5a31cb226acd0ae537e303b56d3f1e6
jolyon.wharton@webenabled.tv <Bobafet1234>
2023-03-05 12:11:48 +0000

    Baby Steps
    Almost feel into the trap of taking too larger steps
    and looking at the journey but we need to start with
    the ship.  Next we'll lok at making the test green
    within the test.

---
+++ EventSourceTests/ShippingTest.cs
@@ -0,0 +1,13 @@
+namespace EventSourceTests
+{
+    public class ShippingTest
+    {
+        [Fact]
+        public void CanCreateNewShip()
+        {
+            var ship = new Ship("Ever Given");
+
+            Assert.
+        }
+    }
+}
